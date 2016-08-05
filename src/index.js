4a89f11bd754008e9667d8ba7944606364792d2c
Edward Shih <ultralabed@gmail.com>
2016-08-05 16:07:38 +0800

    Lecture 21, add Youtube api.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBM60cIHnx43EOeRMxLQfhx9i-s1JpjKwA';
 
+YTSearch({key: API_KEY, term: 'surfboards'}, data => console.log(data) );
 // Create a new componet. This compoent should produce
 // some HTML.
 const App = () => { //a type of component a class
