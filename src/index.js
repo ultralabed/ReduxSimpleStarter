bf31677d8ccbd641bd1ac1bfd9259a7491d28d48
Edward Shih <ultralabed@gmail.com>
2016-08-05 14:28:47 +0800

    Lecture 12 Render Targets

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new componet. This compoent should produce
+// some HTML.
+const App = () => { //a type of component a class
+  return <div>Hi!</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's genrated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
