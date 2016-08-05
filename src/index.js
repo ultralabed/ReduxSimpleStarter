e0cbc6c562b5e0160b3f28a00504cc05dab7e682
Edward Shih <ultralabed@gmail.com>
2016-08-05 16:26:54 +0800

    Lecture 23, Props

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search'
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyBM60cIHnx43EOeRMxLQfhx9i-s1JpjKwA';
 
@@ -11,15 +12,16 @@ class App extends Component {
     
     this.state = { videos: [] };
     YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
-       this.setState({ vidoes });
+       this.setState({ videos });
       }
     );
   }
-  
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/>
       </div>
     );
   }
